4a74593987cac6b9c651853dfc694d874d5d4877
Aristide Niyungeko <niyungeko@gmail.com>
2013-04-22 02:12:29 -0700

    Fix bugs for show and tell

--- CAPComposer/views.py
+++ CAPComposer/views.py
@@ -30,30 +30,29 @@ def finish(request):
     area = caplib.Area()
 
     # info
-    infoBlock._urgency = request.POST['urgency']
-    infoBlock._severity = request.POST['severity']
-    infoBlock._certainty = request.POST['certainty']
-    infoBlock._category = request.POST['category']
-    infoBlock._response_type = request.POST['responseType']
-    infoBlock._language = request.POST['language']
-    infoBlock._event = request.POST['event']
-    infoBlock._event = request.POST['event']
-    infoBlock._sender_name = request.POST['senderName']
-    infoBlock._headline = request.POST['headline']
-    infoBlock._description = request.POST['description']
-    infoBlock._instruction = request.POST['instruction']
-    infoBlock._web = request.POST['web']
-    infoBlock._contact = request.POST['contact']
+    infoBlock._urgency = request.POST['urgency'] if request.POST.has_key('urgency') else ''
+    infoBlock._severity = request.POST['severity'] if request.POST.has_key('severity') else ''
+    infoBlock._certainty = request.POST['certainty'] if request.POST.has_key('certainty') else ''
+    infoBlock._category = request.POST['category'] if request.POST.has_key('category') else ''
+    infoBlock._response_type = request.POST['responseType'] if request.POST.has_key('responseType') else ''
+    infoBlock._language = request.POST['language'] if request.POST.has_key('language') else ''
+    infoBlock._event = request.POST['event'] if request.POST.has_key('event') else ''
+    infoBlock._sender_name = request.POST['senderName'] if request.POST.has_key('senderName') else ''
+    infoBlock._headline = request.POST['headline'] if request.POST.has_key('headline') else ''
+    infoBlock._description = request.POST['description'] if request.POST.has_key('description') else ''
+    infoBlock._instruction = request.POST['instruction'] if request.POST.has_key('instruction') else ''
+    infoBlock._web = request.POST['web'] if request.POST.has_key('web') else ''
+    infoBlock._contact = request.POST['contact'] if request.POST.has_key('contact') else ''
     # infoBlock._event_code = request.POST['eventCode']
 
     #area
-    area._area_description = request.POST['areaDesc']
+    area._area_description = request.POST['areaDesc'] if request.POST.has_key('areaDesc') else ''
 
-    circles = request.POST['circleAreas']
-    circlesDataList = json.loads(circles)
+    circles = request.POST['circleAreas'] if request.POST.has_key('circleAreas') else '{}'
+    circlesDataList = json.loads(circles) #if request.POST.has_key('source') else ''
     area.add_multiple_circles(circlesDataList)
 
-    polygons = request.POST['polygonAreas']
+    polygons = request.POST['polygonAreas'] if request.POST.has_key('polygonAreas') else '{}'
     polygonData = json.loads(polygons)
     area.add_multiple_polygons(polygonData)
 
